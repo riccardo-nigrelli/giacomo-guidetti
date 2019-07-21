26344a8db176ae1fa531077b7a397b5154651d2f
Riccardo Nigrelli <20014421@studenti.uniupo.it>
2019-07-21 15:08:57 +0200

    resolver error and conflicting

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,49 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+const cleanCSS = require('gulp-clean-css');
 var browserSync = require('browser-sync').create();
-var cleanCSS = require('gulp-clean-css');
-var rename = require("gulp-rename");
-var uglify = require('gulp-uglify');
-var imagemin = require('gulp-imagemin');
 
-gulp.task('minify-css', function () {
-    return gulp.src('app/dist/css/*.css')
-        .pipe(cleanCSS({
-            compatibility: 'ie8'
-        }))
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest('app/css/'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
+gulp.task('minify-css', () => {
+  return gulp.src('app/dist/css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('app/css'))
 });
 
-gulp.task('minify-js', function () {
-    return gulp.src('app/dist/js/*.js')
-        .pipe(uglify())
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest('app/js/'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
+gulp.task('minify-js', () => {
+  return gulp.src('app/dist/js/*.js')
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('app/js'))
 });
 
-gulp.task('browserSync', function () {
-    browserSync.init({
-        server: {
-            baseDir: './app/'
-        },
-    })
-})
-
-gulp.task('compress-img', function () {
-    gulp.src('app/dist/img/*')
-        .pipe(imagemin({
-            verbose: true
-        }))
-        .pipe(gulp.dest('app/images/'))
+gulp.task('all', () => {
+  gulp.watch('app/dist/css/*.css', gulp.series('minify-css'));
+  gulp.watch('app/dist/js/*.js', gulp.series('minify-js'));
 });
 
-gulp.task('dev', ['browserSync', 'minify-css', 'minify-js'], function () {
-    gulp.watch('app/dist/css/*.css', ['minify-css']);
-    gulp.watch('app/dist/js/*.js', ['minify-js']);
-
-    gulp.watch('app/*.html', browserSync.reload);
-    gulp.watch('app/dist/js/*.js', browserSync.reload);
+gulp.task('img-compress', () => {
+  return gulp.src('app/dist/img/*')
+    .pipe(imagemin({
+      verbose: true
+    }))
+    .pipe(gulp.dest('app/images/'))
 });
 
+gulp.task('browserSync', () => {
+  browserSync.init({
+    server: {
+      baseDir: './app/'
+    },
+  })
+});
 
-gulp.task('auto', function () {
-    gulp.watch('app/dist/css/*.css', ['minify-css']);
-    gulp.watch('app/dist/js/*.js', ['minify-js']);
+gulp.task('dev', gulp.series('browserSync', 'minify-css', 'minify-js'), () => {
+  gulp.watch('app/dist/css/*.css', gulp.series('minify-css'));
+  gulp.watch('app/dist/js/*.js', gulp.series('minify-js'));
+
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/dist/js/*.js', browserSync.reload);
 });
