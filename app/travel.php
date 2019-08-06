ad26b7585d6a3494f32bdf1a5f161ae44e377b71
Riccardo Nigrelli <20014421@studenti.uniupo.it>
2019-08-06 23:51:16 +0200

    travel gallery

---
+++ app/travel.php
@@ -0,0 +1,47 @@
+<section class='gallery-section' style='padding-bottom: 500px;'>
+  <div class='container-fluid grid-custom p-80'>
+    <div class='row'>
+
+      <div style='padding: 4px;' class='text-center col-md-4'>
+        <a href='./albums/travel/Marrakech_tetti.jpg' class='gallery-link'>
+          <img style='padding-bottom: 8px;' src='./albums/travel/Marrakech_tetti.jpg' alt=''
+            class='gallery-image img-fluid'>
+        </a>
+        <a href='./albums/travel/Ricky_travel.jpg' class='gallery-link'>
+          <img src='./albums/travel/Ricky_travel.jpg' alt=''
+            class='gallery-image img-fluid'>
+        </a>
+      </div>
+
+      <div style='padding: 4px;' class='text-center col-md-4'>
+        <a href='./albums/travel/Bergen_fiordi.jpg' class='gallery-link'>
+          <img style='padding-bottom: 8px;' src='./albums/travel/Bergen_fiordi.jpg' alt=''
+            class='gallery-image img-fluid'>
+        </a>
+        <a href='./albums/travel/Marrakech_lampada.jpg' class='gallery-link'>
+          <img src='./albums/travel/Marrakech_lampada.jpg' alt=''
+            class='gallery-image img-fluid'>
+        </a>
+      </div>
+
+      <div style='padding: 4px;' class='text-center col-md-4'>
+        <a href='./albums/travel/Gozo_rocce.jpg' class='gallery-link'>
+          <img style='padding-bottom: 8px;' src='./albums/travel/Gozo_rocce.jpg' alt=''
+            class='gallery-image img-fluid'>
+        </a>
+        <a href='./albums/travel/Bergen_fiordi2.jpg' class='gallery-link'>
+          <img src='./albums/travel/Bergen_fiordi2.jpg' alt=''
+            class='gallery-image img-fluid'>
+        </a>
+      </div>
+    </div>
+
+    <div class='row'>
+      <div style='padding: 4px;' class='text-center col-lg-12 col-md-12 col-sm-12'>
+        <a href='./albums/travel/AURORA_boreale.jpg' class='gallery-link'>
+          <img src='./albums/travel/AURORA_boreale.jpg' alt='' class='gallery-image img-fluid'>
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
