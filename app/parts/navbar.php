f8046a8dff5cd1f4648653c105871afcb5077a29
Riccardo Nigrelli <20014421@studenti.uniupo.it>
2019-08-17 00:13:40 +0200

    change color and add active class

--- app/parts/navbar.php
+++ app/parts/navbar.php
@@ -1,33 +1,28 @@
 <div class="row justify-content-md-center text-center" style="margin-top: 50px; margin-left: 50px; margin-right: 50px;">
   <div class="col-12">
-      <nav class='navbar navbar-expand-lg' id='probootstrap-navbar'>
-          <div class='container'>
-              <a class='navbar-brand' href='/'>
-                  <h2 class="font-cinzel" style="color: grey;">Giacomo Guidetti</h2>
-              </a>
-              <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#menu' aria-controls='menu'
-                  aria-expanded='false' aria-label='Toggle navigation'>
-                  <span class='navbar-toggler-icon span-nav'> </span>
-              </button>
-              <div class='collapse navbar-collapse' id='menu'>
-                  <ul class='navbar-nav ml-auto'>
-                      <li class='nav-item m-r-15'>
-                          <a id='home' class='nav-link' href='/app/'>Home</a>
-                      </li>
-                      <li class='nav-item m-r-15'>
-                          <a id='challenges' class='nav-link' href='portfolio.php'>Portfolio</a>
-                      </li>
-                      <li class='nav-item m-r-15'>
-                          <a id='questions' class='nav-link' href='#'>About</a>
-                      </li>
-                      <li class='nav-item m-r-15'>
-                          <a id='questions' class='nav-link' href='#'>
-                              Contact
-                          </a>
-                      </li>
-                  </ul>
-              </div>
-          </div>
-      </nav>
+    <nav class="navbar navbar-expand-md" style="padding: 0px">
+      <a href="/" class="mobile-centering navbar-brand">
+        <h2 class="font-cinzel" style="color: grey;">Giacomo Guidetti</h2>
+      </a>
+      <button class="mobile-centering text-center navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar7">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="mobile-centering navbar-collapse collapse" id="navbar7" style="padding: 0px">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a id="home" class="<?php echo ($_SERVER['REQUEST_URI'] === "/") ? "nav-link-active" : "nav-link" ?>" href="/">Home</a>
+          </li>
+          <li class="nav-item">
+            <a id="challenges" class="<?php echo ($_SERVER['REQUEST_URI'] === "/portfolio.php") ? "nav-link-active" : "nav-link" ?>" href="portfolio.php">Portfolio</a>
+          </li>
+          <li class="nav-item">
+            <a id="questions" class="<?php echo ($_SERVER['REQUEST_URI'] === "/#") ? "nav-link-active" : "nav-link" ?>" href="#">About</a>
+          </li>
+          <li class="nav-item mm-r-15">
+            <a id="questions" class="<?php echo ($_SERVER['REQUEST_URI'] === "/#") ? "nav-link-active" : "nav-link" ?>" href="#" style="padding-right: 0px;">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   </div>
 </div>
