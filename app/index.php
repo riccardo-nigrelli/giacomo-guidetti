01ea93616f2540c029728502afc233641d260879
Riccardo Nigrelli <20014421@studenti.uniupo.it>
2019-08-16 23:46:33 +0200

    resolved mobile problem

--- app/index.php
+++ app/index.php
@@ -9,22 +9,12 @@
 	<meta name="author" content="">
 	<title>Giacomo Guidetti | Photographer</title>
 	
-	<!-- <link href="css/bootstrap.min.css" rel="stylesheet"> -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<link href="css/theme.min.css" rel="stylesheet">
-	<!-- <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"> -->
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
 	<link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700,400italic,700italic" rel="stylesheet" type="text/css">
 	<link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-	
-	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	
-	<!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-	<![endif]-->
 
 	<style>
 		body, html{
@@ -38,37 +28,19 @@
 
 </head>
 
-<body id="page-top">
-	
-	<!-- <div class=""> -->
+<body>
+  <div class="home container-fluid h-100">
+		<div class="row h-100 justify-content-center align-items-center">
+			<div class="col-md-12 text-center my-auto">
+				<h1 class="font-cinzel font-70 color-white">Giacomo Guidetti</h1>
+				<h4 class="font-cinzel font-24 color-white">PHOTOGRAPHY</h4>
 
-        <div class="home container-fluid">
-            <div class="row h-100">
-				<div class="col-md-12 text-center my-auto">
-					<h1 class="font-cinzel font-70 color-white">Giacomo Guidetti</h1>
-					<h4 class="font-cinzel font-24 color-white">PHOTOGRAPHY</h4>
-
-					<button class="font-18 btn btn-outline-light m-top-60" type="button"
-					onclick="document.location.href='portfolio.php';" >View More</button>
-				</div>
-            </div>
-        
-
-        <div class="row align-items-center" style="left: 50%; position: absolute; transform: translateX(-50%); padding-top: calc(100vh - 380px)">
-
-          <div class="col-md-auto text-center" style="font-size:25px; margin-right: 10px;">
-						<a href="https://www.instagram.com/giacomo__guidetti/" target="_blank"><i style="color: grey;" id="instagram" class="fab fa-instagram"></i></a>
-					</div>
-					<div class="col-md-auto text-center" style="font-size:25px; margin-right: 10px;">
-						<a href="https://www.youtube.com/channel/UCgSxw-jGMHaFckdqYf5m81Q?app=desktop" target="_blank"><i style="color: grey;" id="youtube" class="fab fa-youtube"></i></a>
-					</div>
-					<div class="col-md-auto text-center" style="font-size:25px; margin-right: 10px;">
-						<a href="https://www.facebook.com/giacomo.guidetti.7" target="_blank"><i style="color: grey;" id="facebook" class="fab fa-facebook"></i></a>
-					</div>
-        </div>
-            
-        </div>
-	<!-- </div> -->
+				<button class="font-18 btn btn-outline-light m-top-60" type="button" 
+					onclick="document.location.href='portfolio.php';">View More
+				</button>
+			</div>
+    </div>    
+  </div>
 
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
@@ -77,3 +49,19 @@
 </body>
 
 </html>
+
+<!-- 
+
+	<div class="row align-items-center" style="left: 50%; position: absolute; transform: translateX(-50%); padding-top: calc(100vh - 380px)">
+		<div class="col-md-auto text-center" style="font-size:25px; margin-right: 10px;">
+			<a href="https://www.instagram.com/giacomo__guidetti/" target="_blank"><i style="color: grey;" id="instagram" class="fab fa-instagram"></i></a>
+		</div>
+		<div class="col-md-auto text-center" style="font-size:25px; margin-right: 10px;">
+			<a href="https://www.youtube.com/channel/UCgSxw-jGMHaFckdqYf5m81Q?app=desktop" target="_blank"><i style="color: grey;" id="youtube" class="fab fa-youtube"></i></a>
+		</div>
+		<div class="col-md-auto text-center" style="font-size:25px; margin-right: 10px;">
+			<a href="https://www.facebook.com/giacomo.guidetti.7" target="_blank"><i style="color: grey;" id="facebook" class="fab fa-facebook"></i></a>
+		</div>
+	</div>
+
+ -->
