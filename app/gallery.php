d8d7808391d400898467b848fa420a7bb758fb5d
Riccardo Nigrelli <20014421@studenti.uniupo.it>
2019-08-21 01:18:46 +0200

    actual state

--- app/gallery.php
+++ app/gallery.php
@@ -39,7 +39,6 @@
   ?>
 
   <?php require_once('parts/footer.php') ?>
-  
 
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
     integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
@@ -49,17 +48,30 @@
   </script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
     integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin></script>
-  <script src='https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js'></script>
+  <!-- <script src=''></script> -->
 
   <script>
     $(document).ready((e) => {
       if (/Android|iPhone/i.test(navigator.userAgent)) {
-        $('a.gallery-link').attr('href', '');
+        $('a.gallery-link').removeAttr('href');
       }
     });
   </script>
 
-  <script src="./js/gallery.min.js"></script>
+  <script>
+    var script = document.createElement('script');
+    var script1 = document.createElement('script');
+    script.type='text/javascript';
+    script1.type='text/javascript';
+
+    if(window.matchMedia("(min-width:500px)").matches) {
+      script.src = 'https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js';
+      script1.src = './js/gallery.min.js';
+    }
+
+    document.getElementsByTagName('head')[0].appendChild(script);
+    document.getElementsByTagName('head')[0].appendChild(script1);
+  </script>
 </body>
 
 </html>
