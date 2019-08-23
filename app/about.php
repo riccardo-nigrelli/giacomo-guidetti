2c71e55736bf9b82cff28660458a1e69e7ce067e
Riccardo Nigrelli <20014421@studenti.uniupo.it>
2019-08-24 00:32:34 +0200

    add about page

---
+++ app/about.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Giacomo Guidetti - About</title>
+  <link rel="shortcut icon" href="./images/favicon.ico">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+    integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700,400italic,700italic" rel="stylesheet"
+    type="text/css">
+  <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css'>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
+    integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+  <link href="css/gallery.min.css" rel="stylesheet">
+  <link href="css/theme.min.css" rel="stylesheet">
+
+  <style>
+    .img-fluid:hover{
+      opacity: 1;
+    }
+  </style>
+
+</head>
+
+<body>
+
+  <div class="container-fluid" style="padding: 0px; margin: 0px;">
+    <?php require_once('parts/navbar.php') ?>
+  </div>
+
+  <div class="container-fluid" style="padding: 0px; margin: 0px;">
+    <div class="row justify-content-md-center text-center margin-portfolio-img">
+      <div class="col-md-6 my-auto">
+        <img class="img-fluid my-auto mx-auto d-block" src="./images/about.jpg" alt="Portrait">
+      </div>
+      <div class="col-md-6 my-auto">
+        <br>
+        <h5 style="font-weight: bold;">SOMETHING ABOUT ME</h5><br>
+        <p>My name is Giacomo.<br>
+          Born in Switzerland but raised in a small town in Italy. I graduated from high school without knowing anything
+          about my nearest future. After that I began a degree in biology science. At the beginning of my second year I
+          started to like making videos and taking pictures so I got my first Gopro Hero 5. After lots of selfies,
+          underwater videos and bad looking long exposures I bought my first and actual Sony camera that drove my
+          passion into this world. Now I’m attending my second and last year before getting my MSc in biology applied to
+          human nutrition. As you thought photography is not my main job but beside that I’m still experimenting and
+          learning to be more professional.
+        </p><br>
+        <br>
+        <h5 style="font-weight: bold;">WHY PHOTOGRAPHY IN THE FIRST PLACE?</h5><br>
+        <p>The one thing I can remember across all this time is friends and family saying how I could take a good photo,
+          with the most common phrase being ‘you have a really good eye’.
+          At first I thought maybe they were just bad at understanding what makes a good
+          picture, but then I thought, ‘maybe I am good at this!’. Over the last few years my passion for photography
+          has grown and continues to grow. From the different styles of photography and the different types of equipment
+          to planning, taking the photo and editing it, there is always something to be learnt.
+        </p><br>
+        <p>Please take a moment to have a look through my website; I hope you find a sense of enjoyment on par with what I felt when taking them.</p>
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid" style="padding: 0px; margin: 0px;">
+    <div class="row justify-content-md-center text-center margin-portfolio-img">
+      <div class="col-12 text-center">
+        <h5 style="font-weight: bold;">THE GEAR I USE</h5><br>
+        <ul class="text-center" style="padding: 0px; margin: 0px; display: flex; justify-content: center;">
+          <ol style="padding-left: 0px;">Sony a6300</ol>
+          <ol>Canon 600D</ol>
+          <ol>Sigma 16mm f/1.4</ol>
+          <ol>Sony 50mm f/1.8</ol>
+          <ol>Sony 55-210mm f/4.5-6.3</ol>
+          <ol>Sony basic lens 16-50mm f/3.5-5.6</ol>
+          <ol>Canon 50mm f/1.8</ol>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <?php require_once('parts/footer.php') ?>
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+  </script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+    integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+  </script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+    integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin>
+  </script>
+
+</body>
+
+</html>
