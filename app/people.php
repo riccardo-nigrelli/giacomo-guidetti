c88626d9a5c1c611700688e3a66df8e2b25a468a
Riccardo Nigrelli <20014421@studenti.uniupo.it>
2019-08-24 00:33:04 +0200

    add correct image link for popup gallery

--- app/people.php
+++ app/people.php
@@ -6,25 +6,25 @@
         <a href="./albums/people/Piralla_1.jpg" class="gallery-link">
           <img style="padding-bottom: 8px;" src="./albums/people/Piralla_1.jpg" alt="" class="gallery-image img-fluid">
         </a>
-        <a href="./albums/people/Piralla_1.jpg" class="gallery-link">
+        <a href="./albums/people/IT.jpg" class="gallery-link">
           <img style="padding-bottom: 8px;" src="./albums/people/IT.jpg" alt="" class="gallery-image img-fluid">
         </a>
-        <a href='./albums/people/Piralla_1.jpg' class='gallery-link'>
+        <a href='./albums/people/Chiara.jpg' class='gallery-link'>
           <img style='padding-bottom: 8px;' src='./albums/people/Chiara.jpg' alt='' class='gallery-image img-fluid'>
         </a>
-        <a href='./albums/people/Piralla_1.jpg' class='gallery-link'>
+        <a href='./albums/people/Camilla_2.jpg' class='gallery-link'>
           <img style='padding-bottom: 8px;' src='./albums/people/Camilla_2.jpg' alt='' class='gallery-image img-fluid'>
         </a>
-        <a href='./albums/people/Luca_viale.jpg' class='gallery-link'>
+        <a href='./albums/people/Bellissimo.jpg' class='gallery-link'>
           <img style='padding-bottom: 8px;' src='./albums/people/Bellissimo.jpg' alt='' class='gallery-image img-fluid'>
         </a>
-        <a href='./albums/people/Luca_viale.jpg' class='gallery-link'>
+        <a href='./albums/people/Dalia_2.jpg' class='gallery-link'>
           <img style='padding-bottom: 8px;' src='./albums/people/Dalia_2.jpg' alt='' class='gallery-image img-fluid'>
         </a>
-        <a href='./albums/people/Luca_viale.jpg' class='gallery-link'>
+        <a href='./albums/people/Delpo_paint.jpg' class='gallery-link'>
           <img style='padding-bottom: 8px;' src='./albums/people/Delpo_paint.jpg' alt='' class='gallery-image img-fluid'>
         </a>
-        <a href='./albums/people/Luca_viale.jpg' class='gallery-link'>
+        <a href='./albums/people/Giangi.jpg' class='gallery-link'>
           <img style='padding-bottom: 8px;' src='./albums/people/Giangi.jpg' alt='' class='gallery-image img-fluid'>
         </a>
 
@@ -34,32 +34,32 @@
         <a href="./albums/people/Camilla_1.jpg" class="gallery-link">
           <img style="padding-bottom: 8px;" src="./albums/people/Camilla_1.jpg" alt="" class="gallery-image img-fluid">
         </a>
-        <a href="./albums/people/Camilla_1.jpg" class="gallery-link">
+        <a href="./albums/people/Delpo_ak47.jpg" class="gallery-link">
           <img style="padding-bottom: 8px;" src="./albums/people/Delpo_ak47.jpg" alt="" class="gallery-image img-fluid">
         </a>
-        <a href='./albums/people/Camilla_1.jpg' class='gallery-link'>
+        <a href='./albums/people/Blumi_2.jpg' class='gallery-link'>
           <img style='padding-bottom: 8px;' src='./albums/people/Blumi_2.jpg' alt='' class='gallery-image img-fluid'>
         </a>
-        <a href='./albums/people/Camilla_1.jpg' class='gallery-link'>
+        <a href='./albums/people/Band_1.jpg' class='gallery-link'>
           <img style='padding-bottom: 8px;' src='./albums/people/Band_1.jpg' alt='' class='gallery-image img-fluid'>
         </a>
-        <a href='./albums/people/Camilla_1.jpg' class='gallery-link'>
+        <a href='./albums/people/Giovanni_montagna.jpg' class='gallery-link'>
           <img style='padding-bottom: 8px;' src='./albums/people/Giovanni_montagna.jpg' alt=''
             class='gallery-image img-fluid'>
         </a>
-        <a href='./albums/people/Piralla_1.jpg' class='gallery-link'>
+        <a href='./albums/people/Delpo_3.jpg' class='gallery-link'>
           <img style='padding-bottom: 8px;' src='./albums/people/Delpo_3.jpg' alt='' class='gallery-image img-fluid'>
         </a>
-        <a href='./albums/people/Piralla_1.jpg' class='gallery-link'>
+        <a href='./albums/people/Veronica_1.jpg' class='gallery-link'>
           <img style='padding-bottom: 8px;' src='./albums/people/Veronica_1.jpg' alt='' class='gallery-image img-fluid'>
         </a>
-        <a href='./albums/people/Piralla_1.jpg' class='gallery-link'>
+        <a href='./albums/people/Delpo_4.jpg' class='gallery-link'>
           <img style='padding-bottom: 8px;' src='./albums/people/Delpo_4.jpg' alt='' class='gallery-image img-fluid'>
         </a>
-        <a href='./albums/people/Luca_viale.jpg' class='gallery-link'>
+        <a href='./albums/people/Bellissimo_montagna.jpg' class='gallery-link'>
           <img style='padding-bottom: 8px;' src='./albums/people/Bellissimo_montagna.jpg' alt='' class='gallery-image img-fluid'>
         </a>
-        <a href='./albums/people/Luca_viale.jpg' class='gallery-link'>
+        <a href='./albums/people/Delpo_1.jpg' class='gallery-link'>
           <img style='padding-bottom: 8px;' src='./albums/people/Delpo_1.jpg' alt='' class='gallery-image img-fluid'>
         </a>
       </div>
@@ -68,28 +68,28 @@
         <a href="./albums/people/Luca_viale.jpg" class="gallery-link">
           <img style="padding-bottom: 8px;" src="./albums/people/Luca_viale.jpg" alt="" class="gallery-image img-fluid">
         </a>
-        <a href="./albums/people/Luca_viale.jpg" class="gallery-link">
+        <a href="./albums/people/Veronica_2.jpg" class="gallery-link">
           <img style="padding-bottom: 8px;" src="./albums/people/Veronica_2.jpg" alt="" class="gallery-image img-fluid">
         </a>
-        <a href='./albums/people/Luca_viale.jpg' class='gallery-link'>
+        <a href='./albums/people/Delpo_2.jpg' class='gallery-link'>
           <img style='padding-bottom: 8px;' src='./albums/people/Delpo_2.jpg' alt='' class='gallery-image img-fluid'>
         </a>
-        <a href='./albums/people/Luca_viale.jpg' class='gallery-link'>
+        <a href='./albums/people/Cosplayer_1.jpg' class='gallery-link'>
           <img style='padding-bottom: 8px;' src='./albums/people/Cosplayer_1.jpg' alt=''
             class='gallery-image img-fluid'>
         </a>
-        <a href='./albums/people/Camilla_1.jpg' class='gallery-link'>
+        <a href='./albums/people/Piralla_2.jpg' class='gallery-link'>
           <img style='padding-bottom: 8px;' src='./albums/people/Piralla_2.jpg' alt='' class='gallery-image img-fluid'>
         </a>
-        <a href='./albums/people/Camilla_1.jpg' class='gallery-link'>
+        <a href='./albums/people/Cosplayer_2.jpg' class='gallery-link'>
           <img style='padding-bottom: 8px;' src='./albums/people/Cosplayer_2.jpg' alt=''
             class='gallery-image img-fluid'>
         </a>
-        <a href='./albums/people/Camilla_1.jpg' class='gallery-link'>
+        <a href='./albums/people/Band_2.jpg' class='gallery-link'>
           <img style='padding-bottom: 8px;' src='./albums/people/Band_2.jpg' alt=''
             class='gallery-image img-fluid'>
         </a>
-        <a href='./albums/people/Camilla_1.jpg' class='gallery-link'>
+        <a href='./albums/people/Dalia_4.jpg' class='gallery-link'>
           <img style='padding-bottom: 8px;' src='./albums/people/Dalia_4.jpg' alt=''
             class='gallery-image img-fluid'>
         </a>
