0556ff105dbf46fd525270df071a4f2e5070062f
Riccardo Nigrelli <20014421@studenti.uniupo.it>
2019-08-25 00:56:51 +0200

    add description images

--- app/nature.php
+++ app/nature.php
@@ -4,28 +4,46 @@
 
       <div style='padding: 4px;' class='text-center col-lg-4 col-md-4 col-sm-12'>
         <a href='./albums/nature/cover.jpg' class='gallery-link'>
-          <img style='padding-bottom: 8px;' src='./albums/nature/cover.jpg' alt='' class='gallery-image img-fluid'>
+          <figure class='gallery-image'>
+            <img style='padding-bottom: 8px;' src='./albums/nature/cover.jpg' alt='' class='gallery-image img-fluid'>
+            <figcaption>Common Bulbul in Marrakech</figcaption>
+          </figure>
         </a>
         <a href='./albums/nature/Anatra_arona.jpg' class='gallery-link'>
-          <img src='./albums/nature/Anatra_arona.jpg' alt='' class='img-fluid'>
+          <figure class='gallery-image'>
+            <img src='./albums/nature/Anatra_arona.jpg' alt='' class='img-fluid'>
+            <figcaption>Floating duck in Italy</figcaption>
+          </figure>
         </a>
       </div>
 
       <div style='padding: 4px;' class='text-center col-lg-4 col-md-4 col-sm-12'>
         <a href='./albums/nature/passero.jpg' class='gallery-link'>
-          <img style='padding-bottom: 8px;' src='./albums/nature/passero.jpg' alt='' class='img-fluid'>
+          <figure class='gallery-image'>
+            <img style='padding-bottom: 8px;' src='./albums/nature/passero.jpg' alt='' class='img-fluid'>
+            <figcaption>The Marrakech fauna</figcaption>
+          </figure>
         </a>
         <a href='./albums/nature/fiore.JPG' class='gallery-link'>
-          <img src='./albums/nature/fiore.JPG' alt='' class='img-fluid'>
+          <figure class='gallery-image'>
+            <img src='./albums/nature/fiore.JPG' alt='' class='img-fluid'>
+            <figcaption>Some nature in Gozo</figcaption>
+          </figure>
         </a>
       </div>
 
       <div style='padding: 4px;' class='text-center col-lg-4 col-md-4 col-sm-12'>
         <a href='./albums/nature/cigno.JPG' class='gallery-link'>
-          <img style='padding-bottom: 8px;' src='./albums/nature/cigno.JPG' alt='' class='img-fluid'>
+          <figure class='gallery-image'>
+            <img style='padding-bottom: 8px;' src='./albums/nature/cigno.JPG' alt='' class='img-fluid'>
+            <figcaption>Swan portrait</figcaption>
+          </figure>
         </a>
         <a href='./albums/nature/Pecore.jpg' class='gallery-link'>
-          <img src='./albums/nature/Pecore.jpg' alt='' class='img-fluid'>
+          <figure class='gallery-image'>
+            <img src='./albums/nature/Pecore.jpg' alt='' class='img-fluid'>
+            <figcaption>Family</figcaption>
+          </figure>
         </a>
       </div>
     </div>
@@ -33,7 +51,10 @@
     <div class='row'>
       <div style='padding: 4px;' class='text-center col-lg-12 col-md-12 col-sm-12'>
         <a href='./albums/nature/monti.jpg' class='gallery-link'>
-          <img src='./albums/nature/monti.jpg' alt='' class='gallery-image img-fluid'>
+          <figure class='gallery-image'>
+            <img src='./albums/nature/monti.jpg' alt='' class='gallery-image img-fluid'>
+            <figcaption>Mottarone landscape</figcaption>
+          </figure>
         </a>
       </div>
     </div>
@@ -42,29 +63,47 @@
 
       <div style='padding: 4px;' class='text-center col-lg-4 col-md-4 col-sm-12'>
         <a href='./albums/nature/Scogliera_gozo.jpg' class='gallery-link'>
-          <img style='padding-bottom: 8px;' src='./albums/nature/Scogliera_gozo.jpg' alt=''
-            class='gallery-image img-fluid'>
+          <figure class='gallery-image'>
+            <img style='padding-bottom: 8px;' src='./albums/nature/Scogliera_gozo.jpg' alt=''
+              class='gallery-image img-fluid'>
+            <figcaption>Gozo's salt pools</figcaption>
+          </figure>
         </a>
         <a href='./albums/nature/Capra.jpg' class='gallery-link'>
-          <img src='./albums/nature/Capra.jpg' alt='' class='img-fluid'>
+          <figure class='gallery-image'>
+            <img src='./albums/nature/Capra.jpg' alt='' class='img-fluid'>
+            <figcaption>Animal Farm</figcaption>
+          </figure>
         </a>
       </div>
 
       <div style='padding: 4px;' class='text-center col-lg-4 col-md-4 col-sm-12'>
         <a href='./albums/nature/Pino.jpg' class='gallery-link'>
-          <img style='padding-bottom: 8px;' src='./albums/nature/Pino.jpg' alt='' class='img-fluid'>
+          <figure class='gallery-image'>
+            <img style='padding-bottom: 8px;' src='./albums/nature/Pino.jpg' alt='' class='img-fluid'>
+            <figcaption>Morning dew</figcaption>
+          </figure>
         </a>
         <a href='./albums/nature/Sugo_vaso.jpg' class='gallery-link'>
-          <img src='./albums/nature/Sugo_vaso.jpg' alt='' class='img-fluid'>
+          <figure class='gallery-image'>
+            <img src='./albums/nature/Sugo_vaso.jpg' alt='' class='img-fluid'>
+            <figcaption>He is Sugo</figcaption>
+          </figure>
         </a>
       </div>
 
       <div style='padding: 4px;' class='text-center col-lg-4 col-md-4 col-sm-12'>
         <a href='./albums/nature/Gabbiano.jpg' class='gallery-link'>
-          <img style='padding-bottom: 8px;' src='./albums/nature/Gabbiano.jpg' alt='' class='img-fluid'>
+          <figure class='gallery-image'>
+            <img style='padding-bottom: 8px;' src='./albums/nature/Gabbiano.jpg' alt='' class='img-fluid'>
+            <figcaption>Wandering seagull</figcaption>
+          </figure>
         </a>
         <a href='./albums/nature/Gatto_gozo.jpg' class='gallery-link'>
-          <img src='./albums/nature/Gatto_gozo.jpg' alt='' class='img-fluid'>
+          <figure class='gallery-image'>
+            <img src='./albums/nature/Gatto_gozo.jpg' alt='' class='img-fluid'>
+            <figcaption>Lady's eyes in Gozo</figcaption>
+          </figure>
         </a>
       </div>
     </div>
@@ -72,20 +111,32 @@
     <div class='row'>
       <div style='padding: 4px;' class='text-center col-lg-4 col-md-4 col-sm-12'>
         <a href='./albums/nature/Formazza_neve.jpg' class='gallery-link'>
-          <img style='padding-bottom: 8px;' src='./albums/nature/Formazza_neve.jpg' alt=''
-            class='gallery-image img-fluid'>
+          <figure class='gallery-image'>
+            <img style='padding-bottom: 8px;' src='./albums/nature/Formazza_neve.jpg' alt=''
+              class='gallery-image img-fluid'>
+            <figcaption>Lonely church in Val Formazza</figcaption>
+          </figure>
         </a>
         <a href='./albums/nature/Cane_mottarone.jpg' class='gallery-link'>
-          <img style='padding-bottom: 8px;' src='./albums/nature/Cane_mottarone.jpg' alt='' class='img-fluid'>
+          <figure class='gallery-image'>
+            <img style='padding-bottom: 8px;' src='./albums/nature/Cane_mottarone.jpg' alt='' class='img-fluid'>
+            <figcaption>Doggo snout</figcaption>
+          </figure>
         </a>
       </div>
 
       <div style='padding: 4px;' class='text-center col-lg-4 col-md-4 col-sm-12'>
         <a href='./albums/nature/Passero_marrakech.jpg' class='gallery-link'>
-          <img style='padding-bottom: 8px;' src='./albums/nature/Passero_marrakech.jpg' alt='' class='img-fluid'>
+          <figure class='gallery-image'>
+            <img style='padding-bottom: 8px;' src='./albums/nature/Passero_marrakech.jpg' alt='' class='img-fluid'>
+            <figcaption></figcaption>
+          </figure>
         </a>
         <a href='./albums/nature/Gatti_bosco.jpg' class='gallery-link'>
-          <img src='./albums/nature/Gatti_bosco.jpg' alt='' class='img-fluid'>
+          <figure class='gallery-image'>
+            <img src='./albums/nature/Gatti_bosco.jpg' alt='' class='img-fluid'>
+            <figcaption>Some kittens in the wood</figcaption>
+          </figure>
         </a>
       </div>
 
--- app/travel.php
+++ app/travel.php
@@ -4,34 +4,46 @@
 
       <div style='padding: 4px;' class='text-center col-md-4'>
         <a href='./albums/travel/Marrakech_tetti.jpg' class='gallery-link'>
-          <img style='padding-bottom: 8px;' src='./albums/travel/Marrakech_tetti.jpg' alt=''
-            class='gallery-image img-fluid'>
+          <figure class='gallery-image'>
+              <img style='padding-bottom: 8px;' src='./albums/travel/Marrakech_tetti.jpg' alt='' class='gallery-image img-fluid'>
+              <figcaption>Marrakech's Roofs</figcaption>
+            </figure>
         </a>
         <a href='./albums/travel/Ricky_travel.jpg' class='gallery-link'>
-          <img src='./albums/travel/Ricky_travel.jpg' alt=''
-            class='gallery-image img-fluid'>
+          <figure class='gallery-image'>
+            <img src='./albums/travel/Ricky_travel.jpg' alt='' class='gallery-image img-fluid'>
+            <figcaption>Gozo's journey</figcaption>
+          </figure>
         </a>
       </div>
 
       <div style='padding: 4px;' class='text-center col-md-4'>
         <a href='./albums/travel/Bergen_fiordi.jpg' class='gallery-link'>
-          <img style='padding-bottom: 8px;' src='./albums/travel/Bergen_fiordi.jpg' alt=''
-            class='gallery-image img-fluid'>
+          <figure class='gallery-image'>
+            <img style='padding-bottom: 8px;' src='./albums/travel/Bergen_fiordi.jpg' alt='' class='gallery-image img-fluid'>
+            <figcaption>Norvegian fjords</figcaption>
+          </figure>
         </a>
         <a href='./albums/travel/Marrakech_lampade.jpg' class='gallery-link'>
-          <img src='./albums/travel/Marrakech_lampade.jpg' alt=''
-            class='gallery-image img-fluid'>
+          <figure class='gallery-image'>
+            <img src='./albums/travel/Marrakech_lampade.jpg' alt='' class='gallery-image img-fluid'>
+            <figcaption>Morocco nigths</figcaption>
+          </figure>
         </a>
       </div>
 
       <div style='padding: 4px;' class='text-center col-md-4'>
         <a href='./albums/travel/Gozo_rocce.jpg' class='gallery-link'>
-          <img style='padding-bottom: 8px;' src='./albums/travel/Gozo_rocce.jpg' alt=''
-            class='gallery-image img-fluid'>
+          <figure class='gallery-image'>
+            <img style='padding-bottom: 8px;' src='./albums/travel/Gozo_rocce.jpg' alt='' class='gallery-image img-fluid'>
+            <figcaption>Memento's rocks</figcaption>
+          </figure>
         </a>
         <a href='./albums/travel/Bergen_fiordi2.jpg' class='gallery-link'>
-          <img src='./albums/travel/Bergen_fiordi2.jpg' alt=''
-            class='gallery-image img-fluid'>
+          <figure class='gallery-image'>
+            <img src='./albums/travel/Bergen_fiordi2.jpg' alt='' class='gallery-image img-fluid'>
+            <figcaption>Norse shelter in the fjords</figcaption>
+          </figure>
         </a>
       </div>
     </div>
@@ -39,7 +51,10 @@
     <div class='row'>
       <div style='padding: 4px;' class='text-center col-lg-12 col-md-12 col-sm-12'>
         <a href='./albums/travel/AURORA_boreale.jpg' class='gallery-link'>
-          <img src='./albums/travel/AURORA_boreale.jpg' alt='' class='gallery-image img-fluid'>
+          <figure class='gallery-image'>
+            <img src='./albums/travel/AURORA_boreale.jpg' alt='' class='gallery-image img-fluid'>
+            <figcaption>Northern lights</figcaption>
+          </figure>
         </a>
       </div>
     </div>
@@ -47,12 +62,18 @@
     <div class='row'>
       <div style='padding: 4px;' class='text-center col-lg-6 col-md-6 col-sm-6'>
         <a href='./albums/travel/Bergen_fiordi3.jpg' class='gallery-link'>
-          <img src='./albums/travel/Bergen_fiordi3.jpg' alt='' class='gallery-image img-fluid'>
+          <figure class='gallery-image'>
+            <img src='./albums/travel/Bergen_fiordi3.jpg' alt='' class='gallery-image img-fluid'>
+            <figcaption>Peculiar houses in fjords</figcaption>
+          </figure>
         </a>
       </div>
       <div style='padding: 4px;' class='text-center col-lg-6 col-md-6 col-sm-6'>
         <a href='./albums/travel/Bergen_fiordi4.jpg' class='gallery-link'>
-          <img src='./albums/travel/Bergen_fiordi4.jpg' alt='' class='gallery-image img-fluid'>
+          <figure class='gallery-image'>
+            <img src='./albums/travel/Bergen_fiordi4.jpg' alt='' class='gallery-image img-fluid'>
+            <figcaption>Vikings' river</figcaption>
+          </figure>
         </a>
       </div>
     </div>
