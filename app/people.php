a0c1753195ae26a0482d01e2f5aa8b08cae1e562
Riccardo Nigrelli <20014421@studenti.uniupo.it>
2019-08-25 03:04:36 +0200

    add description images

--- app/people.php
+++ app/people.php
@@ -4,94 +4,168 @@
 
       <div style="padding: 4px;" class="text-center col-md-4">
         <a href="./albums/people/Piralla_1.jpg" class="gallery-link">
-          <img style="padding-bottom: 8px;" src="./albums/people/Piralla_1.jpg" alt="" class="gallery-image img-fluid">
+          <figure class='gallery-image'>
+            <img style="padding-bottom: 8px;" src="./albums/people/Piralla_1.jpg" alt="" class="gallery-image img-fluid">
+            <figcaption>Lake gig in Comabbio</figcaption>
+          </figure>
         </a>
         <a href="./albums/people/IT.jpg" class="gallery-link">
-          <img style="padding-bottom: 8px;" src="./albums/people/IT.jpg" alt="" class="gallery-image img-fluid">
+          <figure class='gallery-image'>
+            <img style="padding-bottom: 8px;" src="./albums/people/IT.jpg" alt="" class="gallery-image img-fluid">
+            <figcaption>"Time to float..."</figcaption>
+          </figure>
         </a>
         <a href='./albums/people/Chiara.jpg' class='gallery-link'>
-          <img style='padding-bottom: 8px;' src='./albums/people/Chiara.jpg' alt='' class='gallery-image img-fluid'>
+          <figure class='gallery-image'>
+            <img style='padding-bottom: 8px;' src='./albums/people/Chiara.jpg' alt='' class='gallery-image img-fluid'>
+            <figcaption>Janaki's Palace</figcaption>
+          </figure>
         </a>
         <a href='./albums/people/Camilla_2.jpg' class='gallery-link'>
-          <img style='padding-bottom: 8px;' src='./albums/people/Camilla_2.jpg' alt='' class='gallery-image img-fluid'>
+          <figure class='gallery-image'>
+            <img style='padding-bottom: 8px;' src='./albums/people/Camilla_2.jpg' alt='' class='gallery-image img-fluid'>
+            <figcaption>Homerun</figcaption>
+          </figure>
         </a>
         <a href='./albums/people/Blumi_1.jpg' class='gallery-link'>
-          <img style='padding-bottom: 8px;' src='./albums/people/Blumi_1.jpg' alt='' class='gallery-image img-fluid'>
+          <figure class='gallery-image'>
+            <img style='padding-bottom: 8px;' src='./albums/people/Blumi_1.jpg' alt='' class='gallery-image img-fluid'>
+            <figcaption>ニンフ</figcaption>
+          </figure>
         </a>
         <a href='./albums/people/Dalia_2.jpg' class='gallery-link'>
-          <img style='padding-bottom: 8px;' src='./albums/people/Dalia_2.jpg' alt='' class='gallery-image img-fluid'>
+          <figure class='gallery-image'>
+            <img style='padding-bottom: 8px;' src='./albums/people/Dalia_2.jpg' alt='' class='gallery-image img-fluid'>
+            <figcaption>↻ uʍop ǝpısdn ↻</figcaption>
+          </figure>
         </a>
         <a href='./albums/people/Delpo_paint.jpg' class='gallery-link'>
-          <img style='padding-bottom: 8px;' src='./albums/people/Delpo_paint.jpg' alt='' class='gallery-image img-fluid'>
+          <figure class='gallery-image'>
+            <img style='padding-bottom: 8px;' src='./albums/people/Delpo_paint.jpg' alt='' class='gallery-image img-fluid'>
+            <figcaption>Fluo fun</figcaption>
+          </figure>
         </a>
         <a href='./albums/people/Giangi.jpg' class='gallery-link'>
-          <img style='padding-bottom: 8px;' src='./albums/people/Giangi.jpg' alt='' class='gallery-image img-fluid'>
+          <figure class='gallery-image'>
+            <img style='padding-bottom: 8px;' src='./albums/people/Giangi.jpg' alt='' class='gallery-image img-fluid'>
+            <figcaption>John G.</figcaption>
+          </figure>
         </a>
 
       </div>
 
       <div style="padding: 4px;" class="text-center col-md-4">
         <a href="./albums/people/Camilla_1.jpg" class="gallery-link">
-          <img style="padding-bottom: 8px;" src="./albums/people/Camilla_1.jpg" alt="" class="gallery-image img-fluid">
+          <figure class='gallery-image'>
+            <img style="padding-bottom: 8px;" src="./albums/people/Camilla_1.jpg" alt="" class="gallery-image img-fluid">
+            <figcaption>Eeny, Meeny, Miny, Moe</figcaption>
+          </figure>
         </a>
         <a href="./albums/people/Delpo_ak47.jpg" class="gallery-link">
-          <img style="padding-bottom: 8px;" src="./albums/people/Delpo_ak47.jpg" alt="" class="gallery-image img-fluid">
+          <figure class='gallery-image'>
+            <img style="padding-bottom: 8px;" src="./albums/people/Delpo_ak47.jpg" alt="" class="gallery-image img-fluid">
+            <figcaption>Post apocalyptic portrait</figcaption>
+          </figure>
         </a>
         <a href='./albums/people/Blumi_2.jpg' class='gallery-link'>
-          <img style='padding-bottom: 8px;' src='./albums/people/Blumi_2.jpg' alt='' class='gallery-image img-fluid'>
+          <figure class='gallery-image'>
+            <img style='padding-bottom: 8px;' src='./albums/people/Blumi_2.jpg' alt='' class='gallery-image img-fluid'>
+            <figcaption>Nymph Cosplay</figcaption>
+          </figure>
         </a>
         <a href='./albums/people/Band_1.jpg' class='gallery-link'>
-          <img style='padding-bottom: 8px;' src='./albums/people/Band_1.jpg' alt='' class='gallery-image img-fluid'>
+          <figure class='gallery-image'>
+            <img style='padding-bottom: 8px;' src='./albums/people/Band_1.jpg' alt='' class='gallery-image img-fluid'>
+            <figcaption>Lake gig in Comabbio v2.0</figcaption>
+          </figure>
         </a>
         <a href='./albums/people/Giovanni_montagna.jpg' class='gallery-link'>
-          <img style='padding-bottom: 8px;' src='./albums/people/Giovanni_montagna.jpg' alt=''
-            class='gallery-image img-fluid'>
+          <figure class='gallery-image'>
+            <img style='padding-bottom: 8px;' src='./albums/people/Giovanni_montagna.jpg' alt='' class='gallery-image img-fluid'>
+            <figcaption>Skinny guy</figcaption>
+          </figure>
         </a>
         <a href='./albums/people/Delpo_3.jpg' class='gallery-link'>
-          <img style='padding-bottom: 8px;' src='./albums/people/Delpo_3.jpg' alt='' class='gallery-image img-fluid'>
+          <figure class='gallery-image'>
+            <img style='padding-bottom: 8px;' src='./albums/people/Delpo_3.jpg' alt='' class='gallery-image img-fluid'>
+            <figcaption>The definition of portrait</figcaption>
+          </figure>
         </a>
         <a href='./albums/people/Veronica_1.jpg' class='gallery-link'>
-          <img style='padding-bottom: 8px;' src='./albums/people/Veronica_1.jpg' alt='' class='gallery-image img-fluid'>
+          <figure class='gallery-image'>
+            <img style='padding-bottom: 8px;' src='./albums/people/Veronica_1.jpg' alt='' class='gallery-image img-fluid'>
+            <figcaption>Shadows and lights</figcaption>
+          </figure>
         </a>
         <a href='./albums/people/Delpo_4.jpg' class='gallery-link'>
-          <img style='padding-bottom: 8px;' src='./albums/people/Delpo_4.jpg' alt='' class='gallery-image img-fluid'>
+          <figure class='gallery-image'>
+            <img style='padding-bottom: 8px;' src='./albums/people/Delpo_4.jpg' alt='' class='gallery-image img-fluid'>
+            <figcaption>Mountain addict</figcaption>
+          </figure>
         </a>
         <a href='./albums/people/Bellissimo_montagna.jpg' class='gallery-link'>
-          <img style='padding-bottom: 8px;' src='./albums/people/Bellissimo_montagna.jpg' alt='' class='gallery-image img-fluid'>
+          <figure class='gallery-image'>
+            <img style='padding-bottom: 8px;' src='./albums/people/Bellissimo_montagna.jpg' alt='' class='gallery-image img-fluid'>
+            <figcaption>Cold but cool</figcaption>
+          </figure>
         </a>
         <a href='./albums/people/Delpo_1.jpg' class='gallery-link'>
-          <img style='padding-bottom: 8px;' src='./albums/people/Delpo_1.jpg' alt='' class='gallery-image img-fluid'>
+          <figure class='gallery-image'>
+            <img style='padding-bottom: 8px;' src='./albums/people/Delpo_1.jpg' alt='' class='gallery-image img-fluid'>
+            <figcaption>NatGeo style</figcaption>
+          </figure>
         </a>
       </div>
 
       <div style="padding: 4px;" class="text-center col-md-4">
         <a href="./albums/people/Luca_viale.jpg" class="gallery-link">
-          <img style="padding-bottom: 8px;" src="./albums/people/Luca_viale.jpg" alt="" class="gallery-image img-fluid">
+          <figure class='gallery-image'>
+            <img style="padding-bottom: 8px;" src="./albums/people/Luca_viale.jpg" alt="" class="gallery-image img-fluid">
+            <figcaption>Sparkling smile</figcaption>
+          </figure>
         </a>
         <a href="./albums/people/Veronica_2.jpg" class="gallery-link">
-          <img style="padding-bottom: 8px;" src="./albums/people/Veronica_2.jpg" alt="" class="gallery-image img-fluid">
+          <figure class='gallery-image'>
+            <img style="padding-bottom: 8px;" src="./albums/people/Veronica_2.jpg" alt="" class="gallery-image img-fluid">
+            <figcaption>Natural elements</figcaption>
+          </figure>
         </a>
         <a href='./albums/people/Delpo_2.jpg' class='gallery-link'>
-          <img style='padding-bottom: 8px;' src='./albums/people/Delpo_2.jpg' alt='' class='gallery-image img-fluid'>
+          <figure class='gallery-image'>
+            <img style='padding-bottom: 8px;' src='./albums/people/Delpo_2.jpg' alt='' class='gallery-image img-fluid'>
+            <figcaption>Snowy freckels</figcaption>
+          </figure>
         </a>
         <a href='./albums/people/Cosplayer_1.jpg' class='gallery-link'>
-          <img style='padding-bottom: 8px;' src='./albums/people/Cosplayer_1.jpg' alt=''
-            class='gallery-image img-fluid'>
+          <figure class='gallery-image'>
+            <img style='padding-bottom: 8px;' src='./albums/people/Cosplayer_1.jpg' alt='' class='gallery-image img-fluid'>
+            <figcaption>Oni's rage</figcaption>
+          </figure>
+            
         </a>
         <a href='./albums/people/Piralla_2.jpg' class='gallery-link'>
-          <img style='padding-bottom: 8px;' src='./albums/people/Piralla_2.jpg' alt='' class='gallery-image img-fluid'>
+          <figure class='gallery-image'>
+            <img style='padding-bottom: 8px;' src='./albums/people/Piralla_2.jpg' alt='' class='gallery-image img-fluid'>
+            <figcaption>Red fury</figcaption>
+          </figure>
         </a>
         <a href='./albums/people/Cosplayer_2.jpg' class='gallery-link'>
-          <img style='padding-bottom: 8px;' src='./albums/people/Cosplayer_2.jpg' alt=''
-            class='gallery-image img-fluid'>
+          <figure class='gallery-image'>
+            <img style='padding-bottom: 8px;' src='./albums/people/Cosplayer_2.jpg' alt='' class='gallery-image img-fluid'>
+            <figcaption>鬼</figcaption>
+          </figure>
         </a>
         <a href='./albums/people/Band_2.jpg' class='gallery-link'>
-          <img style='padding-bottom: 8px;' src='./albums/people/Band_2.jpg' alt=''
-            class='gallery-image img-fluid'>
+          <figure class='gallery-image'>
+            <img style='padding-bottom: 8px;' src='./albums/people/Band_2.jpg' alt='' class='gallery-image img-fluid'>
+            <figcaption>Golden hour sax</figcaption>
+          </figure>
         </a>
         <a href='./albums/people/Dalia_4.jpg' class='gallery-link'>
-          <img style='padding-bottom: 8px;' src='./albums/people/Dalia_4.jpg' alt=''
-            class='gallery-image img-fluid'>
+          <figure class='gallery-image'>
+            <img style='padding-bottom: 8px;' src='./albums/people/Dalia_4.jpg' alt='' class='gallery-image img-fluid'>
+            <figcaption>Nature aerial dance</figcaption>
+          </figure>
         </a>
       </div>
     </div>
