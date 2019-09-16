faca4880e575088b11c34424d55a26bb7b591516
Riccardo Nigrelli <20014421@studenti.uniupo.it>
2019-09-16 21:55:43 +0200

    update

--- app/companies.php
+++ app/companies.php
@@ -1,12 +1,21 @@
-<div class='container-fluid grid-custom p-80'>
+<div id="card-container" class='container-fluid grid-custom p-80'>
   <div class="row justify-content-md-center text-center">
-    <div style="cursor: pointer;" onclick="window.location.href='vineyards.php'" class="col-md-4" style="margin: 30px; padding: 8px; border: 1px solid black;">
+    <div style="cursor: pointer;" onclick="window.location.href=''" class="col-md-4" style="margin: 30px; padding: 8px;">
       <h1 style="font-size: 40px; position: absolute; color: white; z-index: 1; top: 8%; left: 50%; transform: translate(-50%, -50%);">Giami Srl</h1>
       <img src="albums/companies/giami/giami_2.jpg" class="img-fluid"/>
     </div>
-    <div style="cursor: pointer;" onclick="window.location.href='vineyards.php'" class="col-md-4" style="margin: 30px; padding: 8px; border: 1px solid black;">
+    <div style="cursor: pointer;" onclick="window.location.href='gallery.php?type=companies&name=vineyards'" class="col-md-4" style="margin: 30px; padding: 8px;">
       <h1 style="font-size: 40px; position: absolute; color: white; z-index: 1; top: 8%; left: 50%; transform: translate(-50%, -50%);">Vigneti</h1>
       <img src="albums/companies/vigneti/uva_3.jpg" class="img-fluid"/>
     </div>
   </div>
 </div>
+
+<?php 
+
+  if ( $_GET['type'] === 'companies' && $_GET['name'] === 'vineyards' ) {
+    include_once('vineyards.php');
+    echo "<script>$('#card-container').addClass('hidden-div');</script>";
+  }
+
+?>
