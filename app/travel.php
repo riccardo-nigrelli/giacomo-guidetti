575d6176beef9d2314cfe4e387a7390727fb721e
riccardo-nigrelli <20014421@studenti.uniupo.it>
2020-02-28 23:28:45 +0100

    create corfu travel page

--- app/travel.php
+++ app/travel.php
@@ -1,110 +1,37 @@
-<!-- <section class='gallery-section' style='padding-bottom: 500px;'>
-  <div class='container-fluid grid-custom p-80'>
-    <div class='row'>
-
-      <div style='padding: 4px;' class='text-center col-md-4'>
-        <a href='./albums/travel/Marrakech_tetti.jpg' class='gallery-link'>
-          <figure class='gallery-image'>
-              <img style='padding-bottom: 8px;' src='./albums/travel/Marrakech_tetti.jpg' alt='' class='gallery-image img-fluid'>
-              <figcaption>Marrakech's Roofs</figcaption>
-            </figure>
-        </a>
-        <a href='./albums/travel/Ricky_travel.jpg' class='gallery-link'>
-          <figure class='gallery-image'>
-            <img src='./albums/travel/Ricky_travel.jpg' alt='' class='gallery-image img-fluid'>
-            <figcaption>Gozo's journey</figcaption>
-          </figure>
-        </a>
-      </div>
-
-      <div style='padding: 4px;' class='text-center col-md-4'>
-        <a href='./albums/travel/Bergen_fiordi.jpg' class='gallery-link'>
-          <figure class='gallery-image'>
-            <img style='padding-bottom: 8px;' src='./albums/travel/Bergen_fiordi.jpg' alt='' class='gallery-image img-fluid'>
-            <figcaption>Norvegian fjords</figcaption>
-          </figure>
-        </a>
-        <a href='./albums/travel/Marrakech_lampade.jpg' class='gallery-link'>
-          <figure class='gallery-image'>
-            <img src='./albums/travel/Marrakech_lampade.jpg' alt='' class='gallery-image img-fluid'>
-            <figcaption>Morocco nigths</figcaption>
-          </figure>
-        </a>
-      </div>
-
-      <div style='padding: 4px;' class='text-center col-md-4'>
-        <a href='./albums/travel/Gozo_rocce.jpg' class='gallery-link'>
-          <figure class='gallery-image'>
-            <img style='padding-bottom: 8px;' src='./albums/travel/Gozo_rocce.jpg' alt='' class='gallery-image img-fluid'>
-            <figcaption>Memento's rocks</figcaption>
-          </figure>
-        </a>
-        <a href='./albums/travel/Bergen_fiordi2.jpg' class='gallery-link'>
-          <figure class='gallery-image'>
-            <img src='./albums/travel/Bergen_fiordi2.jpg' alt='' class='gallery-image img-fluid'>
-            <figcaption>Norse shelter in the fjords</figcaption>
-          </figure>
-        </a>
-      </div>
-    </div>
-
-    <div class='row'>
-      <div style='padding: 4px;' class='text-center col-lg-12 col-md-12 col-sm-12'>
-        <a href='./albums/travel/AURORA_boreale.jpg' class='gallery-link'>
-          <figure class='gallery-image'>
-            <img src='./albums/travel/AURORA_boreale.jpg' alt='' class='gallery-image img-fluid'>
-            <figcaption>Northern lights</figcaption>
-          </figure>
-        </a>
-      </div>
-    </div>
-
-    <div class='row'>
-      <div style='padding: 4px;' class='text-center col-lg-6 col-md-6 col-sm-6'>
-        <a href='./albums/travel/Bergen_fiordi3.jpg' class='gallery-link'>
-          <figure class='gallery-image'>
-            <img src='./albums/travel/Bergen_fiordi3.jpg' alt='' class='gallery-image img-fluid'>
-            <figcaption>Peculiar houses in fjords</figcaption>
-          </figure>
-        </a>
-      </div>
-      <div style='padding: 4px;' class='text-center col-lg-6 col-md-6 col-sm-6'>
-        <a href='./albums/travel/Bergen_fiordi4.jpg' class='gallery-link'>
-          <figure class='gallery-image'>
-            <img src='./albums/travel/Bergen_fiordi4.jpg' alt='' class='gallery-image img-fluid'>
-            <figcaption>Vikings' river</figcaption>
-          </figure>
-        </a>
-      </div>
-    </div>
-  </div>
-</section> -->
-
 <div id="card-container" class='container-fluid grid-custom p-80'>
-  <div class="row justify-content-md-center text-center">
+  <!-- <div class="row justify-content-md-center text-center">
     <div style="cursor: pointer;" onclick="window.location.href='#'" class="col-md-4" style="margin: 30px; padding: 8px;">
       <h1 class="font-giami title-company-card">Marrakech</h1>
-      <img src="albums/companies/giami/giami_2.jpg" class="img-fluid"/>
+      <img src="albums/travel/giami/giami_2.jpg" class="img-fluid"/>
     </div>
-    <div style="cursor: pointer;" onclick="window.location.href='gallery.php?type=companies&name=vineyards'" class="col-md-4 margin-top-mobile" style="margin: 30px; padding: 8px;">
+    <div style="cursor: pointer;" onclick="window.location.href='#'" class="col-md-4 margin-top-mobile" style="margin: 30px; padding: 8px;">
       <h1 class="font-card-company title-company-card">Gozo</h1>
-      <img src="albums/companies/vigneti/uva_3.jpg" class="img-fluid"/>
+      <img src="albums/travel/vigneti/uva_3.jpg" class="img-fluid"/>
     </div>
-  </div>
-  <div class="row justify-content-md-center text-center m-top-30">
+  </div> -->
+  <!-- <div class="row justify-content-md-center text-center m-top-30">
     <div style="cursor: pointer;" onclick="window.location.href='#'" class="col-md-4" style="margin: 30px; padding: 8px;">
       <h1 class="font-giami title-company-card">Bergen</h1>
-      <img src="albums/companies/giami/giami_2.jpg" class="img-fluid"/>
+      <img src="albums/travel/giami/giami_2.jpg" class="img-fluid"/>
     </div>
-    <div style="cursor: pointer;" onclick="window.location.href='gallery.php?type=companies&name=vineyards'" class="col-md-4 margin-top-mobile" style="margin: 30px; padding: 8px;">
-      <h1 class="font-card-company title-company-card">Corfu</h1>
-      <img src="albums/companies/vigneti/uva_3.jpg" class="img-fluid"/>
+    <div style="cursor: pointer;" onclick="window.location.href='gallery.php?type=travel&trip=corfu'" class="col-md-4 margin-top-mobile" style="margin: 30px; padding: 8px;">
+      <h1 class="font-card-company title-company-card">Corfù</h1>
+      <img src="albums/travel/Corfu/barchetta.jpg" class="img-fluid"/>
     </div>
-  </div>
+  </div> -->
   <div class="row justify-content-md-center text-center m-top-30">
-    <div style="cursor: pointer;" onclick="window.location.href='#'" class="col-md-4" style="margin: 30px; padding: 8px;">
-      <h1 class="font-giami title-company-card">Cina</h1>
-      <img src="albums/companies/giami/giami_2.jpg" class="img-fluid"/>
+    <div style="cursor: pointer;" onclick="window.location.href='gallery.php?type=travel&trip=corfu'" class="col-md-4 margin-top-mobile" style="margin: 30px; padding: 8px;">
+      <h1 class="font-card-company title-company-card">Corfù</h1>
+      <img src="albums/travel/Corfu/barchetta.jpg" class="img-fluid"/>
     </div>
   </div>
 </div>
+
+<?php
+
+  if ( $_GET['type'] === 'travel' && $_GET['trip'] === 'corfu' ) {
+    include_once('corfu.php');
+    echo "<script>$('#card-container').addClass('hidden-div');</script>";
+  }
+
+?>
