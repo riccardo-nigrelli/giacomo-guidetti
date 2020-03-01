8f905d3307719c1937ac42a53d84ccbeb6c3d340
riccardo-nigrelli <20014421@studenti.uniupo.it>
2020-03-01 23:31:26 +0100

    create new content

---
+++ app/marrakech.php
@@ -0,0 +1,78 @@
+<section class='gallery-section' style='padding-bottom: 500px;'>
+  <div class='container-fluid grid-custom p-80' style="margin-bottom: -400px;">
+    <div class='row'>
+
+      <div style='padding: 4px;' class='text-center col-md-4'>
+        <a href='./albums/travel/Marrakech/mercato.jpg' class='gallery-link'>
+          <figure class='gallery-image'>
+            <img src='./albums/travel/Marrakech/mercato.jpg' alt='' class='gallery-image img-fluid'>
+          </figure>
+        </a>
+
+        <a href='./albums/travel/Marrakech/rosso.jpg' class='gallery-link'>
+          <figure style="margin-top: 8px;" class='gallery-image'>
+            <img src='./albums/travel/Marrakech/rosso.jpg' alt='' class='gallery-image img-fluid'>
+          </figure>
+        </a>
+        
+        <a href='./albums/travel/Marrakech/tetti.jpg' class='gallery-link'>
+          <figure style="margin-top: 8px;" class='gallery-image'>
+            <img src='./albums/travel/Marrakech/tetti.jpg' alt='' class='gallery-image img-fluid'>
+          </figure>
+        </a>
+      </div>
+
+      <div style='padding: 4px;' class='text-center col-md-4'>
+        <a href='./albums/travel/Marrakech/cicogna.jpg' class='gallery-link'>
+          <figure class='gallery-image'>
+            <img src='./albums/travel/Marrakech/cicogna.jpg' alt='' class='gallery-image img-fluid'>
+          </figure>
+        </a>
+      
+        <a href='./albums/travel/Marrakech/cappelli.jpg' class='gallery-link'>
+          <figure style='margin-top: 8px;' class='gallery-image'>
+            <img src='./albums/travel/Marrakech/cappelli.jpg' alt='' class='gallery-image img-fluid'>
+          </figure>
+        </a>
+        
+        <a href='./albums/travel/Marrakech/Uccello_marrakech.jpg' class='gallery-link'>
+          <figure style="margin-top: 8px;" class='gallery-image'>
+            <img src='./albums/travel/Marrakech/Uccello_marrakech.jpg' alt='' class='gallery-image img-fluid'>
+          </figure>
+        </a>
+
+        <a href='./albums/travel/Marrakech/Uccello_verde.jpg' class='gallery-link'>
+          <figure style="margin-top: 8px;" class='gallery-image'>
+            <img src='./albums/travel/Marrakech/Uccello_verde.jpg' alt='' class='gallery-image img-fluid'>
+          </figure>
+        </a>
+      </div>
+      
+      <div style='padding: 4px;' class='text-center col-md-4'>
+        <a href='./albums/travel/Marrakech/moschea.jpg' class='gallery-link'>
+          <figure class='gallery-image'>
+            <img src='./albums/travel/Marrakech/moschea.jpg' alt='' class='gallery-image img-fluid'>
+          </figure>
+        </a>
+
+        <a href='./albums/travel/Marrakech/giallo.jpg' class='gallery-link'>
+          <figure style='margin-top: 8px;' class='gallery-image'>
+            <img src='./albums/travel/Marrakech/giallo.jpg' alt='' class='gallery-image img-fluid'>
+          </figure>
+        </a>
+        
+        <a href='./albums/travel/Marrakech/albergo.jpg' class='gallery-link'>
+          <figure style="margin-top: 8px;" class='gallery-image'>
+            <img src='./albums/travel/Marrakech/albergo.jpg' alt='' class='gallery-image img-fluid'>
+          </figure>
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script>
+  $('#card-container').addClass('hidden-div');
+</script>
